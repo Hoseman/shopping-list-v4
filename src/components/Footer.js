f2fa1e1aee17ec2361c1fcea336c0da63e3bbb84
Andrew Hosegood <andrew.hosegood@Xigens-MacBook-Pro.local>
2023-10-04 17:23:08 +0100

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import styles from '../style.module.css';
 function Footer() {
     return (
         <div className={ styles.footer }>
-            <p className={ styles.footer__signoff }>Shopping List | ©{new Date().getFullYear()} <a href="https://achcreative.co.uk">achcreative</a></p>
+            <p className={ styles.footer__signoff }>Shopping List v1.0 | ©{new Date().getFullYear()} <a href="https://achcreative.co.uk">achcreative</a></p>
         </div>
     );
 }
